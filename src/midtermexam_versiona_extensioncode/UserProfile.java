de105b0abf3954fdb42d000e32aef0222e5f9f7b
HarshDeep100 <160610147+HarshDeep100@users.noreply.github.com>
2024-02-20 12:09:35 -0500

    I have made changes in the git file

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package midtermexam_versiona_extensioncode;
 
-/**
- * The user profile for the OnlineVids system. The user has a userID
- * and a favourite genre that must be one of the choices in the
- * genres array. This code is to be used for SYST 17796 midterm exam.
- * Students are reminded to add themselves as modifiers when editing the
- * code.
- * @author dancye, 2019
- */
+
+// I am adding the comments in the github 
+// My name is Harshdeep
+// My student id is 991719200
+
 public class UserProfile 
 {
     
